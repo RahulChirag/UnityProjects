11d45329ddd44d297be7bd8fbeb807bf904ca700
RahulChirag <chiragrahul222@gmail.com>
2023-08-03 10:45:23 +0530

    gg

--- Assets/Scripts/ObjectScript.cs
+++ Assets/Scripts/ObjectScript.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class ObjectScript : MonoBehaviour
@@ -10,7 +8,7 @@ public class ObjectScript : MonoBehaviour
 
     private void Awake()
     {
-
+        name = transform.gameObject.name.Split("(Clone)")[0];
     }
     // Start is called before the first frame update
     void Start()
