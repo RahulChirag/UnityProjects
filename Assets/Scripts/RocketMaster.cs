7f5e38a97f55576dff0bbbb9807f86c787a82202
RahulChirag <chiragrahul222@gmail.com>
2023-08-03 12:46:25 +0530

    probability of spawning

--- Assets/Scripts/RocketMaster.cs
+++ Assets/Scripts/RocketMaster.cs
@@ -14,7 +14,7 @@ public class RocketMaster : MonoBehaviour
     public List<string> toSpell;
     public List<char> charList;
 
-    public float spawnDelay = 1.0f; // Delay between spawns in seconds  
+    public float spawnDelay = 3.0f; // Delay between spawns in seconds  
 
     private Coroutine spawnCoroutine;
     private int index = 0;
@@ -31,8 +31,9 @@ public class RocketMaster : MonoBehaviour
     void Start()
     {
         // Start the coroutine to spawn objects with a delay
-        spawnCoroutine = StartCoroutine(SpawnRandomObjectsWithDelay());
         SeperateSpelling(index);
+        spawnCoroutine = StartCoroutine(SpawnRandomObjectsWithDelay());
+        
     }
 
     // Update is called once per frame
@@ -43,12 +44,35 @@ public class RocketMaster : MonoBehaviour
 
     private IEnumerator SpawnRandomObjectsWithDelay()
     {
+        // Create a weighted list to include both prefabs and characters from charList
+        List<object> weightedList = new List<object>();
+
+        // Add prefabs to the weighted list with a weight of 1
+        foreach (RectTransform prefabToCheck in prefab)
+        {
+            weightedList.Add(prefabToCheck);
+        }
+
+        // Add characters from charList to the weighted list with a weight based on their occurrences
+        foreach (char character in charList)
+        {
+            int weight = 0;
+            foreach (RectTransform prefabToCheck in prefab)
+            {
+                if (prefabToCheck.GetComponent<ObjectScript>().name == character.ToString())
+                {
+                    weight++;
+                }
+            }
+
+            for (int i = 0; i < weight; i++)
+            {
+                weightedList.Add(character);
+            }
+        }
+
         while (prefab.Count > 0)
         {
-            // Get a random object from the list
-            int randomIndex = Random.Range(0, prefab.Count);
-            RectTransform obj = prefab[randomIndex];
-
             // Calculate random y position within the player area bounds
             Vector2 minBoundary = (Vector2)playerArea.rect.min + new Vector2(playerArea.position.x, playerArea.position.y);
             Vector2 maxBoundary = (Vector2)playerArea.rect.max + new Vector2(playerArea.position.x, playerArea.position.y);
@@ -56,14 +80,24 @@ public class RocketMaster : MonoBehaviour
             float offset = 500f;
             Vector3 spawnPosition = new Vector3(maxBoundary.x + offset, randomY, 0f);
 
-            // Instantiate the object prefab at the calculated position
-            RectTransform spawnedObject = Instantiate(obj, spawnPosition, Quaternion.identity);
-            spawnedObject.SetParent(playerArea); // Make sure the object is a child of the player area
+            // Get a random object from the weighted list
+            int randomIndex = Random.Range(0, weightedList.Count);
+            object spawnObject = weightedList[randomIndex];
+
+            // If the object is a prefab, spawn it
+            if (spawnObject is RectTransform prefabToSpawn)
+            {
+                // Instantiate the object prefab at the calculated position
+                RectTransform spawnedObject = Instantiate(prefabToSpawn, spawnPosition, Quaternion.identity);
+                spawnedObject.SetParent(playerArea); // Make sure the object is a child of the player area
+            }
 
             // Wait for the specified delay before spawning the next object
             yield return new WaitForSeconds(spawnDelay);
         }
     }
+
+
 
     void SeperateSpelling(int index)
     {
